a3a82b58f9725624ce853722f45467500b88c285
“YuliiaDr” <yudragomaretskaya@email.com>
2023-04-01 03:23:54 +0300

    AJAX

--- ajax.js
+++ ajax.js
@@ -1,17 +1,15 @@
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '539ac9544bmsh9038ba6e494a847p1708e4jsne58ebb42946d',
-// 		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-// 	}
-// };
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '539ac9544bmsh9038ba6e494a847p1708e4jsne58ebb42946d',
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+	}
+};
 
-// fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-
+fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
 const myApiKey = "34731faf";
 let currentPage = 1;
